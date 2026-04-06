468641a8a4f4cebe94ecf815effa5044191e6515
Mahfuzur Rahman <mrsabbir1020@gmail.com>
2026-04-06 18:07:46 +0600

    4nd Commit

--- src/main/java/com/example/postofficesystem/Mahfuz/counterClerkDashboardController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/counterClerkDashboardController.java
@@ -1,7 +1,46 @@
 package com.example.postofficesystem.Mahfuz;
 
+import javafx.event.ActionEvent;
+
 public class counterClerkDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void chargeCalculator(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void parcelBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void transactionReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackParcelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void stampSalesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void moneyOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void letterRegistrationOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/postofficesystem/Mahfuz/letterRegistrationViewController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/letterRegistrationViewController.java
@@ -1,7 +1,34 @@
 package com.example.postofficesystem.Mahfuz;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class letterRegistrationViewController
 {
     @javafx.fxml.FXML
+    private ComboBox letterTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField senderNameTextField;
+    @javafx.fxml.FXML
+    private TextField receiverNameTextField;
+    @javafx.fxml.FXML
+    private Label registrationLetterChargeTextArea;
+    @javafx.fxml.FXML
+    private TextField receiverAddressTextField;
+    @javafx.fxml.FXML
+    private Label registrationNumberTextArea;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registrationLetterOnAction(ActionEvent actionEvent) {
+    }
+}
