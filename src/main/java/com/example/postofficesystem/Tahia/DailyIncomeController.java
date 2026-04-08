5549a302ab74a45949be996ff56295cb0ed5f068
Tahia Akter <tahiaakter97@gmail.com>
2026-04-09 01:13:36 +0600

    update

--- src/main/java/com/example/postofficesystem/Tahia/DailyIncomeController.java
+++ src/main/java/com/example/postofficesystem/Tahia/DailyIncomeController.java
@@ -4,4 +4,5 @@ public class DailyIncomeController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+}
--- src/main/java/com/example/postofficesystem/Tahia/PublicProfileSettingsController.java
+++ src/main/java/com/example/postofficesystem/Tahia/PublicProfileSettingsController.java
@@ -4,4 +4,5 @@ public class PublicProfileSettingsController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+}
