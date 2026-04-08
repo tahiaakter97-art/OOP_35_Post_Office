864dd420af8e19f4fcd4d651994e1c3b29e768ad
Mahfuzur Rahman <mrsabbir1020@gmail.com>
2026-04-08 13:27:44 +0600

    4nd Commit

--- src/main/java/com/example/postofficesystem/Mahfuz/cancelBookingViewController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/cancelBookingViewController.java
@@ -1,7 +1,27 @@
 package com.example.postofficesystem.Mahfuz;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class cancelBookingViewController
 {
     @javafx.fxml.FXML
+    private TextField trackingIdTextField;
+    @javafx.fxml.FXML
+    private Label currentStatusTextArea;
+    @javafx.fxml.FXML
+    private Label resultMessageTextArea;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void checkStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cacncelBookingOnAction(ActionEvent actionEvent) {
+    }
+}
