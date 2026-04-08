0372033eec179f888eb1f4c760e1101051f8ee30
Mahfuzur Rahman <mrsabbir1020@gmail.com>
2026-04-09 02:12:48 +0600

    6th  Commit

--- src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
@@ -1,7 +1,14 @@
 package com.example.postofficesystem.Mahfuz;
 
+import javafx.event.ActionEvent;
+
 public class postmanDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void tryOnAction(ActionEvent actionEvent) {
+    }
+}
