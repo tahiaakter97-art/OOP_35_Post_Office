70ad85c16607d563d38bb36db049ed90d6957dad
Mahfuzur Rahman <mrsabbir1020@gmail.com>
2026-04-10 03:37:30 +0600

    resolve

--- src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
@@ -1,21 +1,16 @@
 package com.example.postofficesystem.Mahfuz;
 
-<<<<<<< HEAD
-=======
 import javafx.event.ActionEvent;
 
->>>>>>> 653528260922d9cb924cf7e48caf2d44297edc5d
-public class postmanDashboardController
-{
+public class postmanDashboardController {
+
     @javafx.fxml.FXML
     public void initialize() {
-<<<<<<< HEAD
-    }}
-=======
+
     }
 
     @javafx.fxml.FXML
     public void tryOnAction(ActionEvent actionEvent) {
+
     }
 }
->>>>>>> 653528260922d9cb924cf7e48caf2d44297edc5d
