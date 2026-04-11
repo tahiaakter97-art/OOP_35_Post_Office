8530a57260306ae3a2caffdb648bb8d17c8ec16c
Mahfuzur Rahman <mrsabbir1020@gmail.com>
2026-04-11 19:19:19 +0600

    8th  Commit

--- src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
+++ src/main/java/com/example/postofficesystem/Mahfuz/postmanDashboardController.java
@@ -9,8 +9,44 @@ public class postmanDashboardController {
 
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void tryOnAction(ActionEvent actionEvent) {
 
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportDamagedParcelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rescheduleDeliveryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deliveryReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAssignedParcelsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void returnParcelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deliveryFailedOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markDeliveredOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void collectSignatureOnAction(ActionEvent actionEvent) {
     }
 }
